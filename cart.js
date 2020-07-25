d53a03270db28d6b3ae8ffb2235710ed3a3a7193
Jon Corbett <corbett.jo@husky.neu.edu>
2020-07-25 10:57:54 -0700

    Core functionality: quantity, remove, totals

--- cart.js
+++ cart.js
@@ -1,3 +1,11 @@
+
+
+
+// ---- INIT VALUES ----- //
+
+// Sales tax in percentage (0-100)
+const TAX = 0;
+
 // Array containing user's cart items
 // TODO: Explore an img field for v1.1? (depends on how/where imgs are stored in server)
 var cart = [
@@ -21,37 +29,101 @@ var cart = [
   }
 ]
 
-// Creates and populates <table> with cart data from an arrray
+
+// ----- TABLE POPULATION ----- //
+
+// Creates and populates inventory <table> with cart data from an arrray
 // tID (Number): id of the table that will be populated
 // carrArr (Array): array containing a user's cart items
-function populateTable(tID, cartArr){
+function populateInventory() {
 
   let tHTML = "";
 
-  // Headers
-  tHTML += "<tr>";
-  tHTML += "<th/>"; 
-  tHTML += "<th>Product Name</th>"; 
-  tHTML += "<th>Price</th>";
-  tHTML += "<th>Quantity</th>";
-  tHTML += "<th>Remove</th>";
-  tHTML += "</tr>";
+  // Emtpy cart notif
+  if (cart.length === 0) {
+    tHTML += "<div>Your cart is empty</div>";
+  }
+  // Normal inventory population
+  else {
+    tHTML += "<table>";
 
-  // Rows
-  for (i = 0; i < cartArr.length; i++) {
-    let itm = cartArr[i];
+    // Headers
     tHTML += "<tr>";
-    tHTML += "<td><img src='" + window.location.href + "/assets/" + itm.id + ".png'/></td>";
-    tHTML += "<td><div>" + itm.name + "</div><div>" + itm.id + "</div></td>";
-    tHTML += "<td>" + itm.price + "</td>";
-    tHTML += "<td>" + itm.quantity + "</td>";
-    tHTML += "<td><button>x</button></td>";
+    tHTML += "<th/>"; 
+    tHTML += "<th>Product Name</th>"; 
+    tHTML += "<th>Price</th>";
+    tHTML += "<th>Quantity</th>";
+    tHTML += "<th>Remove</th>";
     tHTML += "</tr>";
+
+    // Rows
+    for (i = 0; i < cart.length; i++) {
+      let itm = cart[i];
+      tHTML += "<tr>";
+      tHTML += "<td><img src='" + window.location.href + "/assets/" + itm.id + ".png'/></td>";
+      tHTML += "<td><div>" + itm.name + "</div><div>" + itm.id + "</div></td>";
+      tHTML += "<td>" + dollarizer.format(itm.price) + "</td>";
+      tHTML += "<td><input id='quan_"+ i + "' type='number' min='1' value=" + itm.quantity + "></input><button onclick='updateQuan(" + i + ")'>update</button></td>";
+      tHTML += "<td><button onclick='removeItem(" + i + ")'>x</button></td>";
+      tHTML += "</tr>";
+    }
+
+    tHTML += "</table>";
   }
 
   // Update HTML
-  document.getElementById(tID).innerHTML = tHTML;
+  document.getElementById("table").innerHTML = tHTML;
 }
 
+// Populates prices table with subtotal, tax, total
+// carrArr (Array): array containing a user's cart items
+function populatePrices() {
+  let sub = calculateTotal();
+  document.getElementById("sub").innerHTML = dollarizer.format(sub);
+  document.getElementById("tax").innerHTML = dollarizer.format(TAX);
+  document.getElementById("total").innerHTML = dollarizer.format(sub * (1 + TAX / 100));
+}
+
+
+// ----- CART EDITS ----- //
+
+// Updates an item's quantity
+// idx (Number): index of the item in the cart
+// TODO: Sanity checking for non-zero
+function updateQuan(idx) {
+  cart[idx].quantity = document.getElementById('quan_' + idx).value;
+  populatePrices();
+}
+
+// Removes an item from user's cart
+// idx (Number): index of the item in the cart
+// TODO: An 'are you sure' popup?
+function removeItem(idx) {
+  cart.splice(idx, 1);
+  populateInventory();
+  populatePrices();
+}
+
+
+// ----- HELPERS ----- //
+
+// Adds up and returns cart total
+// carrArr (Array): array containing a user's cart items
+// returns -> Number
+function calculateTotal() {
+  let total = 0;
+  for (i = 0; i < cart.length; i++) {
+    total += cart[i].price * cart[i].quantity;
+  }
+  return total;
+}
+
+// Number formatter to show prices in US format
+var dollarizer = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
 // Initial call on page load
-populateTable("table", cart);
+populateInventory();
+populatePrices();
